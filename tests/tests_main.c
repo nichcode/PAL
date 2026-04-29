23d18ad6817224ac8db44ca7acb4e8c6f1e0b61e
nichcode <agbonicholas04@gmail.com>
2026-04-29 09:46:16 +0000

    compute test: remove shader files dependency and embed shader sources and bytecode into source file

--- tests/tests_main.c
+++ tests/tests_main.c
@@ -58,7 +58,7 @@ int main(int argc, char** argv)
 
 #if PAL_HAS_GRAPHICS
     // registerTest(graphicsTest, "Graphics Test");
-    // registerTest(computeTest, "Compute Test");
+    registerTest(computeTest, "Compute Test");
     // registerTest(rayTracingTest, "Ray Tracing Test"); // TODO: add dxil shaders
 #endif // PAL_HAS_GRAPHICS
 
@@ -66,7 +66,7 @@ int main(int argc, char** argv)
     // registerTest(clearColorTest, "Clear Color Test");
     // registerTest(triangleTest, "Triangle Test");
     // registerTest(meshTest, "Mesh Test"); // TODO: add dxil shaders
-    registerTest(textureTest, "Texture Test");
+    // registerTest(textureTest, "Texture Test");
 #endif //
 
     runTests();
