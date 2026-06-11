94df64cf55f00fee11999f50256ddc2bc8e446a3
nichcode <agbonicholas04@gmail.com>
2026-06-11 08:32:40 +0000

    Stop tracking pal_config.h file

--- include/pal/pal_config.h
+++
@@ -1,9 +0,0 @@
-
-// Auto Generated Config Header From pal_config.lua
-// Must not be edited manually
-
-#define PAL_HAS_SYSTEM 1
-#define PAL_HAS_THREAD 0
-#define PAL_HAS_VIDEO 1
-#define PAL_HAS_OPENGL 0
-#define PAL_HAS_GRAPHICS 1
