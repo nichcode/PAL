49f88732c256346f56b43dfb578e65eefcc0a760
nichcode <agbonicholas04@gmail.com>
2026-07-11 18:01:43 +0000

    add a linear allocator

---
+++ src/graphics/pal_linear_allocator.h
@@ -0,0 +1,45 @@
+
+/**
+    PAL - Prime Abstraction Layer
+    Copyright (C) 2025
+    Licensed under the Zlib license. See LICENSE file in root.
+ */
+
+#ifndef _PAL_LINEAR_ALLOCATOR_H
+#define _PAL_LINEAR_ALLOCATOR_H
+
+#include "pal/pal_core.h"
+
+#define align(v, a) (v + a - 1) & ~(a - 1)
+
+typedef struct {
+    uint8_t* memory;
+    uint64_t size;
+    uint64_t offset;
+} PalLinearAllocator;
+
+static void* palLinearAlloc(
+    PalLinearAllocator* allocator, 
+    uint64_t size, 
+    uint64_t alignment)
+{
+    uint64_t offset = align(allocator->offset, alignment);
+    if (offset + size > allocator->size) {
+        // allocate a bigger block
+        void* block = palAllocate(nullptr, allocator->size * 2, 0);
+        if (!block) {
+            return nullptr;
+        }
+
+        memcpy(block, (const void*)allocator->memory, allocator->size);
+        palFree(nullptr, (void*)allocator->memory);
+        allocator->memory = (uint8_t*)block;
+        allocator->size = allocator->size * 2;
+    }
+
+    void* ptr = allocator->memory + offset;
+    allocator->offset = offset + size;
+    return ptr;
+}
+
+#endif // _PAL_LINEAR_ALLOCATOR_H
