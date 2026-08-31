0cdc61746695ad23ec9ceb6997bbfaa11c260151
nichcode <agbonicholas04@gmail.com>
2026-08-31 00:28:03 +0000

    add run-tidy.py

---
+++ .github/workflows/run-tidy.py
@@ -0,0 +1,46 @@
+
+import os
+import json
+import subprocess
+import sys
+from concurrent.futures import ThreadPoolExecutor
+
+def worker(info):
+    file, path = info
+    print(f"Checking: {file}")
+
+    config = os.path.join(path, ".clang-tidy")
+    cmd = ["clang-tidy-22", file, "-p", path, f"--config-file={config}"]
+    subprocess.run(cmd, shell=True)
+
+def main():
+    path = os.path.abspath("../../compile_commands.json")
+    if len(sys.argv) > 1:
+        path = os.path.abspath("compile_commands.json")
+
+    if not os.path.exists(path):
+        print(f"Failed to find compile_commands.json")
+        sys.exit(1)
+
+    buildpath = os.path.dirname(path)
+
+    # Load the files
+    with open(path, "r") as file:
+        commands = json.load(file)
+
+    files = []
+    for entry in commands:
+        files.append(os.path.normpath(entry["file"]))
+
+    print(f"Found {len(files)} targets. Running parallel analysis...")
+
+    workers = []
+    for f in files:
+        workers.append((f, buildpath))
+
+    with ThreadPoolExecutor(max_workers=4) as pool:
+        pool.map(worker, workers)
+
+
+if __name__ == "__main__":
+    main()
