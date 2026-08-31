5442f451a762a44e921c806fd3de5bce022bfd8b
nichcode <agbonicholas04@gmail.com>
2026-08-31 20:41:47 +0000

    remove src

--- abi-dump/abi-dump.lua
+++ abi-dump/abi-dump.lua
@@ -5,20 +5,6 @@ project "abi-dump"
     targetdir(targetDir)
     objdir(objDir)
 
-    files {
-        "core_abi_dump.c",
-        "event_abi_dump.c",
-        "thread_abi_dump.c",
-        "system_abi_dump.c",
-        "video_abi_dump.c",
-        "opengl_abi_dump.c",
-        "graphics_abi_dump.c",
-        "abi_dump_main.c"
-    }
-
-    includedirs { 
-        "%{wks.location}/include",
-        "%{wks.location}/tools/abi_dump" 
-    }
-    
+    files { "src/**.c" }
+    includedirs { "%{wks.location}/include" }
     links { "PAL2" }
