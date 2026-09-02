546be00ac61c1ff58ae2000b8aa13faa40367756
nichcode <agbonicholas04@gmail.com>
2026-09-02 16:25:03 +0000

    update pal-gdb.py to include bitmask

--- scripts/pal-gdb.py
+++ scripts/pal-gdb.py
@@ -2,9 +2,13 @@
 import gdb # type: ignore
 
 sets = {}
+bitmask = {}
 
-def addSet(name, values):
+def add_set(name, values):
     sets[name] = values
+
+def add_bitmask(name, values):
+    bitmask[name] = values
 
 class SetPrinter:
     def __init__(self, value, values):
@@ -14,18 +18,65 @@ class SetPrinter:
     def to_string(self):
         value = int(self.value)
         return self.values.get(value, str(value))
+    
+class BitmaskPrinter:
+    def __init__(self, value, values):
+        self.value = value
+        self.values = values
+
+    def to_string(self):
+        value = int(self.value)
+        if value == 0:
+            return self.values.get(0, "0")
+        
+        names = []
+        unknowns = []
+
+        for bit in range(value.bit_length()):
+            mask = 1 << bit
+            if not (value & mask):
+                continue
+
+            name = self.values.get(mask)
+            if name is not None:
+                names.append(name)
+            else:
+                unknowns.append(mask)
+        
+        for bit in unknowns:
+            names.append(f"0x{bit:x}")
+
+        return " | ".join(names)
+
+        # for bit, name in self.values.items():
+        #     print(f"bit={bit} name={name}")
+        #     if bit != 0 and value & bit:
+        #         print(f"bit={bit} name={name}")
+        #         names.append(name)
+
+        # return " | ".join(names)
 
 def lookup(value):
-    values = sets.get(str(value.type))
-    if values is None:
-        return None
+    type_name = str(value.type)
 
-    return SetPrinter(value, values)
+    if type_name in sets:
+        return SetPrinter(value, sets[type_name])
+    
+    if type_name in bitmask:
+        return BitmaskPrinter(value, bitmask[type_name])
+    
+    return None
 
-addSet("Status", {
+add_set("Status", {
     0: "STATUS_GOOD",
     1: "STATUS_BAD",
     2: "STATUS_NORMAL",
+})
+
+add_bitmask("Result", {
+    0: "RESULT_SUCCESS",
+    1 << 0: "RESULT_FAILED_IO",
+    1 << 1: "RESULT_FAILED_HANDLE",
 })
 
 gdb.pretty_printers.append(lookup)
--- tests/src/core/test_result.c
+++ tests/src/core/test_result.c
@@ -1,12 +1,20 @@
 
 #include <stdio.h>
+#include <stdint.h>
 
 typedef int Status;
+typedef uint32_t Result;
 
 // TODO: remove after testing formatter script for gdb and lldg
 #define STATUS_GOOD 0
 #define STATUS_BAD 1
 #define STATUS_NORMAL 2
+
+#define RESULT_SUCCESS 0
+#define RESULT_FAILED_IO (1 << 0)
+#define RESULT_FAILED_HANDLE (1 << 1)
+#define UNKNOWN1 (1 << 2)
+#define UNKNOWN2 (1 << 3)
 
 // The formatter should show the value like an enum not the literal
 int main(void)
@@ -15,9 +23,15 @@ int main(void)
     Status bad =  STATUS_BAD;
     Status normal =  STATUS_NORMAL;
 
+    Result result1 = RESULT_SUCCESS;
+    Result result2 = RESULT_FAILED_IO | RESULT_FAILED_HANDLE | UNKNOWN1 | UNKNOWN2;
+
     printf("%u\n", good);
     printf("%u\n", bad);
     printf("%u\n", normal);
+
+    printf("%u\n", result1);
+    printf("%u\n", result2);
     (void)fflush(stdout);
 
     return 0;
