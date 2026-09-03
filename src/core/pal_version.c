bb06e312943f12e013dc1253303ae5941d6be260
nichcode <agbonicholas04@gmail.com>
2026-09-03 21:21:29 +0000

    add version implementation

---
+++ src/core/pal_version.c
@@ -0,0 +1,45 @@
+
+/**
+ * PAL - Prime Abstraction Layer (PAL)
+ * A cross platform abstraction layer over graphics and windowing APIs
+ * -------------------------------------------------------------------
+ * 
+ * Copyright (C) 2025-2026 Nicholas Agbo <agbonicholas04@gmail.com>
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#include "pal2/pal_core.h"
+
+#define VERSION_MAJOR 2
+#define VERSION_MINOR 2
+#define VERSION_BUILD 0
+#define VERSION_STRING "2.2.0"
+
+void PAL_CALL palGetVersion(PalVersion* version)
+{
+    version->major = VERSION_MAJOR;
+    version->minor = VERSION_MINOR;
+    version->build = VERSION_BUILD;
+}
+
+const char* PAL_CALL palGetVersionString()
+{
+    return VERSION_STRING;
+}
