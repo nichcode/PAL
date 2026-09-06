c45433fb3d067aca7ac99db9aa0fe23e3b955cc5
nichcode <agbonicholas04@gmail.com>
2026-09-06 18:04:40 +0000

    add gen-docs.py script

---
+++ scripts/gen-docs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import subprocess
+from pathlib import Path
+
+def main():
+    root_dir = Path(__file__).resolve().parents[1]
+    build_path = root_dir / "build"
+    build_path.mkdir(parents=True, exist_ok=True)
+
+    os.chdir(root_dir / "docs")
+    subprocess.run(["doxygen", "doxyfile"])
+
+    log_file = build_path / "doxygen.log"
+    with open(log_file, "r") as file:
+        if not file:
+            print(f"Failed to load doxygen.log: {log_file}")
+            sys.exit(1)
+
+        if not file.read(1) == "":
+            print(f"There are errors with the documentation generation")
+            sys.exit(1)
+
+    print(f"")
+    print(f"Documentation generated successfully")
+    return 0
+
+if __name__ == "__main__":
+    main()
